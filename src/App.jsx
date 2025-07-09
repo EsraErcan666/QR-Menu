d51b7b36819255306fc5a28b305c58003d121c97
ComputerUni <m.kadir-27@hotmail.com>
2025-07-09 20:24:04 +0300

    degisiklik3

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-
-import './App.css'
-import Login from './components/Login'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './components/Home';
+import Basket from "./components/Basket.jsx";
 
 function App() {
-
-
-  return (
-    <>
-     <Login/>
-    </>
-  )
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/basket" element={<Basket />} />
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
