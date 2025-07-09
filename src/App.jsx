b4ba7af25f8f1fff11154cc2566d49abb6735710
ComputerUni <m.kadir-27@hotmail.com>
2025-07-09 20:00:45 +0300

    degisiklik2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 
 import './App.css'
 import Login from './components/Login'
+import Home from './components/Home'
 
 function App() {
 
 
   return (
     <>
-     <Login/>
+     <Home/>
     </>
   )
 }
