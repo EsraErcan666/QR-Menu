d5ecd8bbdd3757ac37c09c21a11377e90f4740fd
EsraErcan <esraercan888@gmail.com>
2025-07-09 20:22:12 +0300

    Login3

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../css/Login.css";
 import KadirImg from "../assets/Kadir.jpg";
 
-const Login = ({ onShowMenu }) => {
+const Login = () => {
   return (
     <div className="login-bg">
       <div className="login-card">
@@ -13,7 +13,7 @@ const Login = ({ onShowMenu }) => {
         />
         <h2 className="login-title">QREATIONE</h2>
         <p className="login-welcome">Hoşgeldiniz</p>
-        <button className="login-btn primary" onClick={onShowMenu}>Menüyü Göster</button>
+        <button className="login-btn primary">Menüyü Göster</button>
         <button className="login-btn secondary">Garson Çağır</button>
         <p className="login-footer">Qreatione.com.tr</p>
       </div>
