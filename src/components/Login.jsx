1d2de9a55e8b4bda747d7f20f1d90663865231b5
EsraErcan <esraercan888@gmail.com>
2025-07-11 19:14:08 +0300

    Login responsive

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../css/Login.css";
-import KadirImg from "../assets/Kadir.jpg";
+import LogoImg from "../assets/logo.jpg";
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
@@ -9,15 +9,19 @@ const Login = () => {
     <div className="login-bg">
       <div className="login-card">
         <img
-          src={KadirImg}
-          alt="Kadir"
+          src={LogoImg}
+          alt="Logo"
           className="login-logo"
         />
         <h2 className="login-title">QREATIONE</h2>
         <p className="login-welcome">Hoşgeldiniz</p>
-        <button className="login-btn primary" onClick={() => navigate('/home')}>Menüyü Göster</button>
-        <button className="login-btn secondary">Garson Çağır</button>
-        <p className="login-footer">Qreatione.com.tr</p>
+        <div className="login-btn-group">
+          <button className="login-btn primary" onClick={() => navigate('/home')}>Menüyü Göster</button>
+          <button className="login-btn secondary">Garson Çağır</button>
+        </div>
+        <div className="login-footer-wrapper">
+          <p className="login-footer">Qreatione.com.tr</p>
+        </div>
       </div>
     </div>
   );
