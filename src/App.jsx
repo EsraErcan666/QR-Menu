a1c3c331b9f50fa7d02ac96ab377863368e52497
İrem TUNALI <116944418+iremtunali@users.noreply.github.com>
2025-07-11 21:17:04 +0300

    sepet menuye atama

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from "react";
-import Basket from "./components/Basket";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './components/Home';
+import Basket from "./components/Basket.jsx";
 
 function App() {
     return (
-        <div className="App">
-            <Login />
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/basket" element={<Basket />} />
+            </Routes>
+        </Router>
     );
 }
 
