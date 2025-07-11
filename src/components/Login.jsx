87ca4fb5e6f6d60eadfb208a66f411f5b2ffb538
EsraErcan <esraercan888@gmail.com>
2025-07-11 15:00:59 +0300

    sayfalar arası geçiş

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "../css/Login.css";
 import KadirImg from "../assets/Kadir.jpg";
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const navigate = useNavigate();
   return (
     <div className="login-bg">
       <div className="login-card">
@@ -13,7 +15,7 @@ const Login = () => {
         />
         <h2 className="login-title">QREATIONE</h2>
         <p className="login-welcome">Hoşgeldiniz</p>
-        <button className="login-btn primary">Menüyü Göster</button>
+        <button className="login-btn primary" onClick={() => navigate('/home')}>Menüyü Göster</button>
         <button className="login-btn secondary">Garson Çağır</button>
         <p className="login-footer">Qreatione.com.tr</p>
       </div>
