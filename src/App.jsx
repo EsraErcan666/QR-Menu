498cdeb07b4814762f0c69d66e5bc4c4a253faf8
EsraErcan <esraercan888@gmail.com>
2025-07-13 17:11:58 +0300

    YAS YAS GRE

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './components/Home';
 import Basket from "./components/Basket.jsx";
+import ProductList from "./components/Product.jsx";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Route path="/" element={<Login />} />
                 <Route path="/home" element={<Home />} />
                 <Route path="/basket" element={<Basket />} />
+                <Route path="/product" element={<ProductList />} />
             </Routes>
         </Router>
     );
