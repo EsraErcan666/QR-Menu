7fe52c36a0ddb2426095538b867c7921d59db329
İrem TUNALI <116944418+iremtunali@users.noreply.github.com>
2025-07-15 14:23:40 +0300

    urunleri sepete ekleme

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Home from './components/Home';
-import Basket from "./components/Basket.jsx";
-import ProductList from "./components/Product.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CartProvider } from "./context/CartContext.jsx"; // 📌 eklenen satır
+
+import Home from "./components/Home";
+import Product from "./components/Product";
+import Basket from "./components/Basket";
+import Login from "./components/Login";
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/home" element={<Home />} />
-                <Route path="/basket" element={<Basket />} />
-                <Route path="/product" element={<ProductList />} />
-            </Routes>
-        </Router>
+        <CartProvider> {/* 📌 Tüm uygulamayı sar */}
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/product" element={<Product />} />
+                    <Route path="/basket" element={<Basket />} />
+                    <Route path="/login" element={<Login />} />   // İsteğe bağlı
+                </Routes>
+            </BrowserRouter>
+        </CartProvider>
     );
 }
 
